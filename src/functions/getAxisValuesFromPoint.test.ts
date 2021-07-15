3a14a895ffca6358b19dc8b3ebe0c73fcf7947a4
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-14 22:05:02 -0400

    test getAxisValuesFromPoint function

---
+++ src/functions/getAxisValuesFromPoint.test.ts
@@ -0,0 +1,20 @@
+import { getAxisValuesFromPoint } from "./getAxisValuesFromPoint";
+
+describe(`The ${getAxisValuesFromPoint.name} function`, () => {
+  it("converts a Point into an array of numeric axis values.", () => {
+    [
+      { point: [], expectedAxisValues: [] },
+      { point: [{ axis: "foo", value: 0 }], expectedAxisValues: [0] },
+      {
+        point: [
+          { axis: "bar", value: 0.5 },
+          { axis: "baz", value: 1 },
+        ],
+        expectedAxisValues: [0.5, 1],
+      },
+    ].forEach(({ point, expectedAxisValues }) => {
+      const axisValues = getAxisValuesFromPoint(point);
+      expect(axisValues).toStrictEqual<number[]>(expectedAxisValues);
+    });
+  });
+});
